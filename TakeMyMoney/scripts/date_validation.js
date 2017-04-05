30fbb640819cdaae4b7ddc6850b6bbc4752f717a
Teddy Hartanto <hartantoteddy@yahoo.com>
2017-04-06 00:41:29 +0800

    Added alert to base, modified date_validation.js

--- TakeMyMoney/scripts/date_validation.js
+++ TakeMyMoney/scripts/date_validation.js
@@ -2,14 +2,17 @@ function date_validation(){
     var startDate = document.getElementById("start_date").value;
     var endDate = document.getElementById("end_date").value;
 
+    startDate = startDate == "" ? null : new Date(startDate);
+    endDate = endDate == "" ? null : new Date(endDate);
+
     var message
 
-    if(!(startDate < endDate)) {
-        $("#date_checker").html("Funding start date must be before end date");
+    if (startDate !== null && endDate !== null && !(startDate < endDate)) {
+        $("#date_error").css("display", "block");
         document.getElementById("submit").disabled = true;
     }
     else {
-        $("#date_checker").html("");
+        $("#date_error").css("display", "none");
         document.getElementById("submit").disabled = false;
     }
 }
